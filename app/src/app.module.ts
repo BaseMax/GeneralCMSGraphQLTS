0c17462131a5f120f975a23fad463135bae1600e
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-07-10 22:36:07 +0330

    add graphQL module

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,10 +1,13 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver : ApolloDriver ,
+      playground : false ,
+    })
+  ] ,
 })
 export class AppModule {}
--- app/src/app.query.ts
+++
@@ -1,9 +0,0 @@
-import { Field, ObjectType } from "@nestjs/graphql";
-
-@ObjectType()
-export class Query {
-    @Field()
-    hello():string {
-        return 'hello world'
-    }
-}
