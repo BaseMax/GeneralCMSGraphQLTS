8bf12d922208ecb670a726dba82ecf463774163b
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-07-11 14:29:30 +0330

    chnage app directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver : ApolloDriver ,
-      playground : false ,
-    })
+    GraphQLModule.forRoot({
+      autoSchemaFile : true ,
+
+    }) , 
+    UserModule
   ] ,
 })
 export class AppModule {}
