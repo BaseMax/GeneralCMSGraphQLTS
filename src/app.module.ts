62076043112783fc1f10cd7d727672bb11017d02
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-07-11 19:07:41 +0330

    add other modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,26 @@
+import { ApolloServerPluginLandingPageGraphQLPlayground } from '@apollo/server-plugin-landing-page-graphql-playground';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DatabaseModule } from './config/database.module';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
-      autoSchemaFile : true ,
-
-    }) , 
-    UserModule
-  ] ,
+    // GraphQLModule.forRoot<ApolloDriverConfig>({
+    //   driver : ApolloDriver,
+    //   autoSchemaFile : join(process.cwd() , "src/schema.gql"),
+    //   playground : false , 
+    //   plugins : [ApolloServerPluginLandingPageGraphQLPlayground()]
+    // }) , 
+    DatabaseModule , 
+    UserModule , 
+  ] , 
+  controllers : [AppController] , 
+  providers : [AppService]
 })
 export class AppModule {}
