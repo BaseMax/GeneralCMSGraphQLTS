c9756e71d4550c63539304e9e83df1a708f56b67
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-07-11 19:08:03 +0330

    create user controller for test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UserService } from './user/user.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService ,
+    private readonly userService:UserService 
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getAllUsers(){
+    return await this.userService.findAll()
   }
 }
