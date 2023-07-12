8de67f9eded5886f4bac07ac96f42d867a2e67c5
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-07-12 21:55:11 +0330

    add create user mutation

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,18 +1,32 @@
-import { Resolver , Query } from "@nestjs/graphql";
-import { UserEntity } from "./entities/user.entity";
+import { Resolver , Query, Args, Mutation } from "@nestjs/graphql";
+import { CreateUserInput } from "./dto/create-user.input";
+import { User } from "./entities/user.entity";
 import { UserService } from "./user.service";
 
 
-@Resolver(()=>UserEntity)
+@Resolver(()=>User)
 export class UserReslover {
     constructor(
         private readonly userService:UserService 
     ){}
 
-    @Query(()=>[UserEntity] , {name : 'getAllUsers'})
-    async getAllUsers(){
+    @Query(()=>[User] , {name : 'findAllUser'})
+    async findAllUsers(){
         const users = await this.userService.findAll();
 
         return users ; 
+    }
+
+    @Query(()=> User , {name : 'findUserById'})
+    async findUserById(@Args("id" , {type : ()=>String}) id : string){
+        return await this.userService.findOne({id});
+    }
+
+
+    @Mutation(()=>User)
+    async createUser(
+        @Args("createUserInput") createUserInput:CreateUserInput 
+    ){
+        return this.userService.create(createUserInput);
     }
 }
