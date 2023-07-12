b76fc3487db29982ce87f38bb333a8f007775a39
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-07-12 21:55:37 +0330

    add create user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,29 +1,64 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { FindOptionsWhere, Repository } from "typeorm";
-import { UserEntity } from "./entities/user.entity";
+import { CreateUserInput } from "./dto/create-user.input";
+import { User  } from "./entities/user.entity";
+import { Role } from "./enums/role.enum";
 
-type Where = FindOptionsWhere<UserEntity>
+type Where = FindOptionsWhere<User>
 
 @Injectable()
 export class UserService {
     constructor(
-        @InjectRepository(UserEntity)
-        private readonly userRepo:Repository<UserEntity>
+        @InjectRepository(User)
+        private readonly userRepo:Repository<User>
     ){}
 
-    async findAll():Promise<UserEntity[]>{
+    async findAll():Promise<User[]>{
         return await this.userRepo.find()
     }
 
-    async findOne(where:Where):Promise<UserEntity>{
-        const user = await this.userRepo.findOne({where}) ;
-
-        if(!user){
-            throw new NotFoundException('user not found')
-        }
-
-        return user ; 
+    async findOne(where:Where):Promise<User>{
+        return await this.userRepo.findOne({where}) ; 
     }
 
+    async create(createUserInput:CreateUserInput):Promise<User>{
+        let { 
+            firstName , 
+            lastName , 
+            email , 
+            password , 
+            username
+        } = createUserInput ;
+
+    
+        const userInDb = await this.findOne({email});
+        const usernameInDb = await this.findOne({username});
+        
+        if(userInDb){
+            throw new BadRequestException('this is email alredy exist');
+        }
+
+        if(usernameInDb){
+            throw new BadRequestException('this is username alredy exist');
+        }            
+
+        // add admin role to first user registration
+        const userCount: number = await this.userRepo.count();
+        let roles:Role[] = [Role.User];
+
+        if (userCount==0){
+             roles.push(Role.Admin);
+        }
+
+        const user = new User()
+        user.email = email ;
+        user.username = username ; 
+        user.password = password ;
+        user.firstName = firstName ;
+        user.lastName = lastName ;
+        user.roles = roles;
+        
+        return this.userRepo.save(user);
+    }
 }
