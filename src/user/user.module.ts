fd186924500f0aa3de2b791556b047046dc14a22
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-07-12 21:54:39 +0330

    modified

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,12 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { UserEntity } from "./entities/user.entity";
+import { User } from "./entities/user.entity";
 import { UserReslover } from "./user.resolver";
 import { UserService } from "./user.service";
 
 @Module({
     imports : [
-        TypeOrmModule.forFeature([UserEntity])
+        TypeOrmModule.forFeature([User])
     ] ,
     providers : [UserService , UserReslover] ,
     exports : [UserService]
