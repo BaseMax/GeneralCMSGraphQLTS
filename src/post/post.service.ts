f0cdd259865160d29f3226ddea0b22fcaca80b56
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-07-18 23:03:00 +0330

    .

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,7 +1,29 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable} from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { FindOptionsWhere, Repository } from "typeorm";
+import { Post } from "./entity/post.entity";
 
+
+type Where = FindOptionsWhere<Post>
 
 @Injectable()
 export class PostService {
-    
+    constructor(
+        @InjectRepository(Post)
+        private readonly postRepo:Repository<Post>
+    ){}
+
+    async findOne(where:Where):Promise<Post>{
+        return await this.postRepo.findOne({where})
+    }
+
+    async findAllPost():Promise<Post[]>{
+        return await this.postRepo.find({})
+    }
+
+    async create(createPostInput){}
+
+    async update(){}
+
+    async delete(){}
 }
