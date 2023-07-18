ab30f2e54caa5a4623130ac4e6ad5095d8103a6b
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-07-18 22:49:17 +0330

    add other module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,23 +4,28 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './config/database.module';
+import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
+import { CategoryModule } from './category/category.module';
+import { FaqModule } from './faq/faq.module';
+import { MenuPositionModule } from './menu-position/menu-position.module';
 
 @Module({
   imports: [
     DatabaseModule ,
     GraphQLModule.forRoot<ApolloDriverConfig>({
+      context: ({ req  }) => ({ req }),
       driver : ApolloDriver,
       autoSchemaFile : join(process.cwd() , "src/schema.gql"),
       playground : false , 
-      plugins : [ApolloServerPluginLandingPageGraphQLPlayground()]
+      plugins : [ApolloServerPluginLandingPageGraphQLPlayground()] , 
     }) , 
     UserModule , 
     AuthModule , 
+    PostModule, CategoryModule, FaqModule, MenuPositionModule
   ] , 
-  providers : [AppService] ,
+  providers : [] ,
 })
 export class AppModule {}
