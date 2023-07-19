15a8871faaffb127d263a2f31c88a5a8f727e8f2
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-07-19 23:17:56 +0330

    clean unknwon code

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,6 +1,8 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Entity } from 'typeorm';
 
 @ObjectType()
+// @Entity({name : "Category"})
 export class Category {
   @Field(() => Int, { description: 'Example field (placeholder)' })
   exampleField: number;
