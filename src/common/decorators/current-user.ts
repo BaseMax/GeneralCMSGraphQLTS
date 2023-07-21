e151e71f2a8bc37af02d04cb862ef5a3a3c1e3dc
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-07-21 14:35:13 +0330

    for access user

--- src/common/decorators/current-user.ts
+++
@@ -1,8 +0,0 @@
-import { createParamDecorator } from "@nestjs/common";
-
-export const CurrentUser = createParamDecorator(
-    (data, [root, args, ctx, info]) => {
-      return ctx.req.user;
-    },
-);
-  
