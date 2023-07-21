9c9c2e3724aba11657d52950111d26cb9fa356b7
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-07-21 23:19:56 +0330

    add category feature

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryResolver } from './category.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Category } from './entities/category.entity';
 
 @Module({
   imports : [
-    TypeOrmModule.forFeature([CategoryModule]) ,
+    TypeOrmModule.forFeature([Category]) ,
   ] ,
   providers: [CategoryResolver, CategoryService]
 })
