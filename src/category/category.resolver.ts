11a40504dd98a29c473c43c01e8c11d62a39c801
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-07-21 23:25:18 +0330

    sync relovers with services and same output resolver with status-result

--- src/category/category.resolver.ts
+++ src/category/category.resolver.ts
@@ -3,11 +3,19 @@ import { CategoryService } from './category.service';
 import { Category } from './entities/category.entity';
 import { CreateCategoryInput } from './dto/create-category.input';
 import { UpdateCategoryInput } from './dto/update-category.input';
+import { StatusResult } from './entities/status-result.entity';
+import { RoleGuard } from 'src/auth/guards/role.guard';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { Role } from 'src/user/enums/role.enum';
+import { HasRoles } from 'src/user/decorator/role.decorator';
+import { UseGuards } from '@nestjs/common';
 
 @Resolver(() => Category)
 export class CategoryResolver {
   constructor(private readonly categoryService: CategoryService) {}
 
+  @HasRoles(Role.Admin)
+  @UseGuards(JwtAuthGuard , RoleGuard)
   @Mutation(() => Category)
   createCategory(@Args('createCategoryInput') createCategoryInput: CreateCategoryInput) {
     return this.categoryService.create(createCategoryInput);
@@ -19,17 +27,22 @@ export class CategoryResolver {
   }
 
   @Query(() => Category, { name: 'category' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
+  findOne(@Args('id', { type: () => String }) id: string) {
     return this.categoryService.findOne(id);
   }
 
-  @Mutation(() => Category)
+
+  @HasRoles(Role.Admin)
+  @UseGuards(JwtAuthGuard , RoleGuard)
+  @Mutation(() => StatusResult)
   updateCategory(@Args('updateCategoryInput') updateCategoryInput: UpdateCategoryInput) {
     return this.categoryService.update(updateCategoryInput.id, updateCategoryInput);
   }
 
-  @Mutation(() => Category)
-  removeCategory(@Args('id', { type: () => Int }) id: number) {
+  @HasRoles(Role.Admin)
+  @UseGuards(JwtAuthGuard , RoleGuard)
+  @Mutation(() => StatusResult)
+  removeCategory(@Args('id', { type: () => String }) id: string) {
     return this.categoryService.remove(id);
   }
 }
