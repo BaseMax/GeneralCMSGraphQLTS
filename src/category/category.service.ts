0534735ee453bd73e87b13817c204a21b15b7a17
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-07-21 23:23:17 +0330

    create CRUD logics for category

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { StatusResult } from 'src/common/utils/status-result';
 import { Repository } from 'typeorm';
 import { CreateCategoryInput } from './dto/create-category.input';
 import { UpdateCategoryInput } from './dto/update-category.input';
@@ -12,23 +13,54 @@ export class CategoryService {
     @InjectRepository(Category)
     private readonly categoryRepo:Repository<Category>
   ){}
-  create(createCategoryInput: CreateCategoryInput) {
+  
+  async create(createCategoryInput: CreateCategoryInput):Promise<Category>{
+    const { name } = createCategoryInput ;
+    const categoryInDb = await this.categoryRepo.findOne({where : {name}});
+
+    if(categoryInDb){
+      throw new BadRequestException('this category alredy used');
+    }
+
+    const newCategory = new Category();
+    newCategory.name = name ; 
+
+    return await this.categoryRepo.save(newCategory);
+  }
+
+  async findAll():Promise<Category[]>{
+    return await this.categoryRepo.find({relations : ['posts']}) ;
+  }
+
+  async findOne(id: string):Promise<Category>{
+    return await this.categoryRepo.findOneBy({id});
+  }
+
+  async update(id: string, updateCategoryInput: UpdateCategoryInput):Promise<StatusResult>{
+    const result:StatusResult = {
+      message : 'item edited successfully' , 
+      success : true , 
+    }
+    const { name } = updateCategoryInput ;
+    const category = await this.findOne(id) ;
+
+    if(!category){
+      throw new NotFoundException('category not found');
+    }
+
+    await this.categoryRepo.update({id} , {name})
+
+    return result ; 
+  }
+
+  async remove(id: string):Promise<StatusResult> {
+    let result:StatusResult = {
+      message : 'item deleted successfully' ,
+      success : true ,
+    }
     
-  }
+    await this.categoryRepo.delete({id}) ;
 
-  findAll() {
-    return `This action returns all category`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
-  }
-
-  update(id: number, updateCategoryInput: UpdateCategoryInput) {
-    return `This action updates a #${id} category`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} category`;
+    return result ;
   }
 }
