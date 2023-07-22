424810098326f2a66b862646cf23fed65ace3274
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-07-22 17:46:00 +0330

    add category module

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,12 +1,16 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { CategoryModule } from "src/category/category.module";
 import { Post } from "./entity/post.entity";
 import { PostResolver } from "./post.resolver";
 import { PostService } from "./post.service";
 
 
 @Module({
-    imports : [ TypeOrmModule.forFeature([Post])] , 
+    imports : [ 
+        TypeOrmModule.forFeature([Post]),
+        CategoryModule
+    ] , 
     providers : [PostResolver , PostService] , 
     exports : []
 })
