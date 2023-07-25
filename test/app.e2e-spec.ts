6864b3fd577625386369aef8e556c954a1286e25
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-07-25 19:12:26 +0330

    add tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -6,18 +6,17 @@ import { Repository } from 'typeorm';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
 import { Post } from 'src/post/entity/post.entity';
-import { PassportModule } from '@nestjs/passport';
-import { PassportConf } from 'src/config/passport.config';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
   let userRepo : Repository<User> ; 
   let postRepo : Repository<Post> ; 
+  let token : string ; 
 
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule , PassportModule.register(PassportConf)],
+      imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
@@ -34,7 +33,7 @@ describe('AppController (e2e)', () => {
   describe("auth", () => {
     it("must register", async () => {
       const mutation = `mutation register($registerInput: RegisterInput!) {
-        register(signupInput: $registerInput) {
+        register(registerInput: $registerInput) {
           access_token 
         }
       }`;
@@ -56,23 +55,96 @@ describe('AppController (e2e)', () => {
         });
 
       expect(response.status).toBe(200);
+      
+    });
+
+    it("must login", async () => {
+      const mutation = `mutation login($loginInput: LoginInput!) {
+        login(loginInput: $loginInput) {
+          access_token 
+        }
+      }`;
+      const variables = {
+        registerInput: {
+          username : "john" ,
+          password : "123456", 
+        }
+      };
+
+      const response = await request(app.getHttpServer())
+        .post("/graphql")
+        .send({
+          query: mutation,
+          variables,
+        });
+
+      expect(response.status).toBe(200);
+      token = response.body.data.access_token ;
       });
+  });
+
+
+  describe("book", () => {
+    it("must create book", async () => {
+      const mutation = `mutation createBook($createBookInput: CreateBookInput!) {
+        createBook(createBookInput: $createBookInput) {
+          title , 
+        }
+      }`;
+      const variables = {
+        createBookInput: {
+          title : "talk the animals" ,
+          content : "cats", 
+          fullContent : "talk about cats ",
+          slug : "cat-is-cool" , 
+        }
+      };
+
+      const response = await request(app.getHttpServer())
+        .post("/graphql")
+        .set({Authorization : token})
+        .send({
+          query: mutation,
+          variables,
+        });
+
+      expect(response.status).toBe(200);
+      
+      });
+
+      it("find all books", async () => {
+        const query = `query {
+            findAllPost{
+            title , 
+            content ,
+            fullContent , 
+            slug 
+          }
+      }`
+
+        const response = await request(app.getHttpServer())
+          .post("/graphql")
+          .set({Authorization : token})
+          .send({
+            query: query,
+          });
+  
+        expect(response.status).toBe(200);
+        
+      }); 
     });
 
 
-    describe("book", () => {
-      it("must register", async () => {
-        const mutation = `mutation createBook($createBookInput: CreateBookInput!) {
-          createBook(createBookInput: $createBookInput) {
-            title , 
+    describe("category", () => {
+      it("must create ctategory", async () => {
+        const mutation = `mutation createCategory($createCategory: CreateCategoryInput!) {
+          createCategory(createCategoryInput: $createCategory) {
+            name 
           }
         }`;
         const variables = {
-          createBookInput: {
-            title : "talk the animals" ,
-            content : "cats", 
-            fullContent : "talk about cats ",
-            slug : "cat-is-cool" , 
+          categoryInput : {
+           name : "cat"
           }
         };
   
@@ -85,8 +157,23 @@ describe('AppController (e2e)', () => {
   
         expect(response.status).toBe(200);
         
-        });
       });
-
-      
+  
+      it("find All category", async () => {
+        const query = `query {
+          findAllCategory {
+            name 
+          }
+        }`;
+  
+        const response = await request(app.getHttpServer())
+          .post("/graphql")
+          .send({
+            query: query
+          });
+  
+        expect(response.status).toBe(200);
+        });
+    });
+    
 });
