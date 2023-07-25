2782514b5fd92845082aec8155b7336a24304b1b
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-07-25 19:11:59 +0330

    add providers

--- src/menu-position/menu-position.module.ts
+++ src/menu-position/menu-position.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MenuPositionService } from './menu-position.service';
 import { MenuPositionResolver } from './menu-position.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MenuPosition } from './entities/menu-position.entity';
 
 @Module({
-  providers: [MenuPositionResolver, MenuPositionService]
+  imports : [ TypeOrmModule.forFeature([MenuPosition]) ],
+  providers: [MenuPositionService ,MenuPositionResolver]
 })
 export class MenuPositionModule {}
