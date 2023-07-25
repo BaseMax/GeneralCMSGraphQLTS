89d79961ab8c40c6227f60be4c3d678af35d75db
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-07-25 19:12:11 +0330

    .

--- src/tag/tag.resolver.ts
+++ src/tag/tag.resolver.ts
@@ -2,7 +2,6 @@ import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
 import { TagService } from './tag.service';
 import { Tag } from './entities/tag.entity';
 import { CreateTagInput } from './dto/create-tag.input';
-import { UpdateTagInput } from './dto/update-tag.input';
 import { StatusResult } from 'src/common/entities/status-result.entity';
 
 @Resolver(() => Tag)
